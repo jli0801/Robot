aeec8c707b7faa7a9a5a797435f559041e31d64d
jessi <jessi@DESKTOP-8072EKE.home>
2017-09-22 20:51:46 -0400

    setup complete

--- Greeting/src/greeting/GreetingClass.java
+++ Greeting/src/greeting/GreetingClass.java
@@ -2,4 +2,27 @@ package greeting;
 
 public class GreetingClass {
 
+	public static final String[] gRes = {"That's good to hear.","Great!",
+			"Wow. Tell me more.","I'm glad it went well.", "That's amazing!"};
+	public static final String[] bRes = {"That's bad news.","I'm so sorry.",
+			"Would you like to talk more about it?",
+			"That's too bad. ", "Your day will get better."};
+	private String userInput;
+	 
+	private int helloCount;
+	
+	public Hello()
+	{
+	 helloCount = 0;
+	}
+	
+	public static boolean keywordIsIsolated(int position ,String keyword ,String s)
+	{
+		
+	}
+	
+	public static boolean noNegations(String s, int psn)
+	{
+		
+	}
 }
