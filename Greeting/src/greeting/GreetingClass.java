313df1f592d0cb278eb321e1e5a1eafb7f6e27ef
Vicky Icky <Vicky Icky@VickyIcky.fios-router.home>
2017-09-24 15:05:39 -0400

    No negation

--- Greeting/src/greeting/GreetingClass.java
+++ Greeting/src/greeting/GreetingClass.java
@@ -48,14 +48,52 @@ public class GreetingClass {
 	
 	public static boolean noNegations(String s, int psn)
 	{
-		/*returns true if there is not a negation ("no", "not") immediately in front of psn.
+		/*returns true if there isn't a negation ("no", "not") immediately in front of psn.
+		 * returns false if there is a negation immediately in front of psn
 		 * PRECONDITION : s is lowercase, psn is within bounds
+		 * EXAMPLES: 
+		 * nN("I am not Great, but I am okay", 9) -> false
+		 * nN("I am not great, but I am Okay", 25) -> true
+		 * nN("okay", 0) -> true
 		 */
 		
 		//if String s == "not" || "no"
-		if (//no negation in front of psn)
+		/*if (//no negation in front of psn)
 		{
 			return true;
+		}*/
+		
+		int negation = s.indexOf("no");
+		
+		if ( negation == -1)
+		{
+			return true
+		} else
+		{
+			//find out if word is no or not
+			if (s.substring(negation+2, negation+3).compareTo("t") == 0) 
+			{
+				//the word is not
+				if (negation + 4 == psn)// it is +4 and not +3 b/c i included the space
+				{
+					return false;
+							
+				} else
+				{
+					return true;
+				}
+			}else
+			{
+				//the word is no
+				if (negation + 3 == psn)
+				{
+					return false;
+							
+				} else
+				{
+					return true;
+				}
+			}
 		}
 	}
 }
