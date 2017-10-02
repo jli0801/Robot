f5403ff688bc79fe648a3688a5f80c2b3a4fc5e1
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-10-02 15:29:18 -0400

    Testing in chabot

--- Greeting/src/chatbot/ChatbotJessica.java
+++ Greeting/src/chatbot/ChatbotJessica.java
@@ -7,48 +7,53 @@ public class ChatbotJessica implements Topic {
 	private String secretKeyword;
 	private String response;
 	
+	
 	public ChatbotJessica() {
 		String[] temp = {"stuff", "things", "whatever", "nothing"};
 		keywords = temp;
+		
 		goodbyeKeyword = "bye";
-		secretKeyword = "corgi";
+		secretKeyword = "pug";
 		response = "";
 	}
 
+	
 	public void talk(String response) {
-		ChatbotMain.print("Hey! So you want to talk about generic boring things? "
-				+ "I love talking about that");
+		
+		ChatbotMain.print("Hey! So you want to talk about generic boring things, huh? I love talking about that. So tell me something");
 		response = ChatbotMain.getInput();
-		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) != -1);
+		while (ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1)
 		{
 			if (ChatbotMain.findKeyword(response, secretKeyword, 0) >= 0)
 			{
-				ChatbotMain.print("I love corgris so much! So cool.");
-			}
-			else
+				ChatbotMain.print("I can't even. I love pugs so much. Wow. You are so cool");
+				response = ChatbotMain.getInput();
+			}else
 			{
-				
-				ChatbotMain.print("That's cool but there are other cooler things");
+				ChatbotMain.print("Yeah. That's pretty cool. but there are things I like even more. Tell me something else");
 				response = ChatbotMain.getInput();
 			}
 		}
-		ChatbotMain.print("Well it was nice talking to you" + ChatbotMain.chatbot.getUsername() + "!");
-		//ChatbotMain.chatbot.getJessica().talk("");;
+		
+		//access variables from other classes
+		ChatbotMain.print("Well, it was nice talking to you," +ChatbotMain.chatbot.getUsername() + "!");
+		//ChatbotMain.chatbot.getVickie().talk("");
 		ChatbotMain.chatbot.startChatting();
 	}
-	
 
-	
+
 	public boolean isTriggered(String response) {
-		for(int i = 0; i < keywords.length; i++)
+		
+		for (int i = 0; i < keywords.length; i++)
 		{
-			//IMPORTANT (one the rubric) only if keyword is used
-			if(ChatbotMain.findKeyword(response, keywords[i], 0) >= 0)
+			//IMPORTANT (on the rubric)
+			if (ChatbotMain.findKeyword(response, keywords[i], 0) >= 0)
 			{
 				return true;
 			}
 		}
 		return false;
+	}
 
 }
-}
+
