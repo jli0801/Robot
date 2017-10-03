4e451275df63c97b5838ad89c11b2659f3f20a6e
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2017-10-03 15:12:50 -0400

    added arrays

--- Greeting/src/chatbot/ChatbotJessica.java
+++ Greeting/src/chatbot/ChatbotJessica.java
@@ -6,20 +6,28 @@ public class ChatbotJessica implements Topic {
 	private String goodbyeKeyword;
 	private String secretKeyword;
 	private String response;
-	private String[] calmRes;
-	private String[] upsetRes;
-	
+	private String[] calmResArr;
+	private String[] upsetResArr;
+	private String[] jokesArr;
+	private String[] jokesAns;
+	private String[] gamesArr; 
 	
 	public ChatbotJessica() {
-		String[] triggerM = {"joke", "fun", "", "", "game"};
+		String[] triggerM = {"joke", "story", "game"};
 		keywords = triggerM;
 		String[] calmRes = {"Please answer my question.", "I don't think you get this. "
 				,"I don't understand.", "I don't think I can help you with that."};
+		calmResArr = calmRes;
 		String[] upsetRes = {"I can't understand you if you won't be specific. ", "Please explain further. "
 				,"I need more details. ", "Do you really understand? "};
-		
-		
+		upsetResArr = upsetRes;
+		String[] jokes = {"What makes oil boil?", ""};
+		jokesArr = jokes;
+		String[] answerJ = {"The letter B!", };		
+		jokesAns = answerJ;
 		goodbyeKeyword = "bye";
+		String[] games = {};
+		gamesArr = games;
 		secretKeyword = "funny";
 		response = "";
 	}
@@ -27,17 +35,27 @@ public class ChatbotJessica implements Topic {
 	
 	public void talk(String response) {
 		
-		ChatbotMain.print("");
+		ChatbotMain.print("Great! You came to the right place.");
 		response = ChatbotMain.getInput();
 		while (ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1)
 		{
 			if (ChatbotMain.findKeyword(response, secretKeyword, 0) >= 0)
 			{
-				ChatbotMain.print("I'm glad you find ");
+				ChatbotMain.print("I'm glad you find me funny! Would you like to here another joke?");
 				response = ChatbotMain.getInput();
-			}else
+			}
+			else
 			{
-				ChatbotMain.print("");
+				for (int i = 0; i < keywords.length; i++)
+				{
+					if (ChatbotMain.findKeyword(response, keywords[i], 0) >= 0 )
+					{
+						
+						ChatbotMain.print("Yes of course. I'll tell you a " + keywords[i]+ ". ");
+						
+					}
+				}
+
 				response = ChatbotMain.getInput();
 			}
 		}
@@ -60,6 +78,11 @@ public class ChatbotJessica implements Topic {
 			}
 		}
 		return false;
+	}
+	
+	public String getKeyword(String response)
+	{
+		
 	}
 
 }
