bdd8cb3f581e58cb9cf83e629cc8bae749370b95
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2017-10-05 14:17:58 -0400

    update ji

--- Greeting/src/chatbot/ChatbotJi.java
+++ Greeting/src/chatbot/ChatbotJi.java
@@ -17,7 +17,7 @@ public class ChatbotJi implements Topic{
 	//
 	
 	public ChatbotJi() {
-		String[] temp = {"college", "university", "school", "application", "essay", ""}; //first three links to college chatbot
+		String[] temp = {"college", "university", "school", "application", "essay", "a", "b"}; //first three links to college chatbot
 		String[] happy = {"I'm glad you asked me for help.", "Thanks for showing your interest.", "Your future is bright."};
  		String[] mild = {"Be more specific.", "Something else you want to say?", "I think we have gone over that. Let's talk about something else."};
 		String[] annoyed = {"If you aren't here for college help, might as well google it.", "I have other students who need help, come back when you have prepared reasonable questions.", "I don't have all day."};
@@ -28,7 +28,7 @@ public class ChatbotJi implements Topic{
 	}
 
 	public void talk(String response) {
-		ChatbotMain.print("By any chance, are you interested in learning what certain colleges are available for you?");
+		ChatbotMain.print("By any chance, are you interested in applying for college? If so, what?");
 		response = ChatbotMain.getInput();
 		
 		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) != -1);
@@ -50,20 +50,17 @@ public class ChatbotJi implements Topic{
 							ChatbotMain.print("Well.. if you need help, you can always me.");
 						}
 					}
- 				}
- 				
- 				if (ChatbotMain.findKeyword(response, keywords[6], 0) >= 0) { //link scores/gpa
+ 				} else if (ChatbotMain.findKeyword(response, keywords[6], 0) >= 0) { //link scores/gpa
  					ChatbotMain.print("According to your scores, your eligibility is determined by the percentage your college accepts based on your score ranges.");
 					//essay
 					//extracurriculars
- 				}
- 				
- 				if (ChatbotMain.findKeyword(response, keywords[3], 0) >= 0) { //assist with college app
+ 				} else if (ChatbotMain.findKeyword(response, keywords[3], 0) >= 0) { //assist with college app
  					ChatbotMain.print("What part of the college app do you need help with?");
 					
  				} else {
 	 				ChatbotMain.print("I'm not quite sure what you need help with. Would you mind rephrasing your question?");
  				}
+				
 				response = ChatbotMain.getInput();
 			}
 		}
