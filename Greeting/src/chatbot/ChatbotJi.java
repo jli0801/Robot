af8e08ec0fcb1d833b3b60047ea1e5b6040c9a7a
yejaz <yejaz@192.168.0.14>
2017-10-12 19:23:55 -0400

    fix

--- Greeting/src/chatbot/ChatbotJi.java
+++ Greeting/src/chatbot/ChatbotJi.java
@@ -152,11 +152,11 @@ public class ChatbotJi implements Topic {
 
 	public void getBotReaction() {
 		if ((numAnnoyed > 1) && (numAnnoyed < 4)) { // mild
-			ChatbotMain.print(mild[(int) Math.floor(Math.random() * 2)]);
+			ChatbotMain.print(mild[(int) Math.floor(Math.random() * 3)]);
 		}
 
 		if (numAnnoyed <= 1) { // annoyed
-			ChatbotMain.print(annoyed[(int) Math.floor(Math.random() * 2)]);
+			ChatbotMain.print(annoyed[(int) Math.floor(Math.random() * 3)]);
 		}
 
 		// if -1 end discussion?
